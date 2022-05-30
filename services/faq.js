b4081aa69e46f9b7eb711a202d0bd211c23368c0
GuillermoCAB <guiga.benitez@gmail.com>
2022-05-30 10:10:10 -0300

    fix: change api to fix typo

--- services/faq.js
+++ services/faq.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "./api";
 export const gatFAQs = async () => {
   try {
     let fetch = await axiosInstance.get(
-      "/8a2117ac-efd0-4dae-8643-e9e975b24743"
+      "/6e61d30e-2e7f-4c08-89fe-5fb7949d59b8"
     );
 
     return fetch.data;
